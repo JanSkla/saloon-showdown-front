0dcc0c76d31c3ae3462c75f984050cd30b03616b
JanSkla <jan.skladal11@gmail.com>
2024-06-22 19:30:27 +0200

    lobby display players

--- src/pages/LobbyPage.jsx
+++ src/pages/LobbyPage.jsx
@@ -1,5 +1,14 @@
-const LobbyPage = () => <div>
-    lobbyPage
-</div>
+import { useContext } from "react";
+import { RoomContext } from "../utilComponents/RoomDataProvider";
+
+const LobbyPage = () => {
+
+    const { players, setPlayers } = useContext(RoomContext);
+
+    return <div>
+        lobbyPage
+        {players.map(player => <div key={player.pId}>id: {player.pId} name: {player.name} {player.isLeadPlayer && "🎖️"}</div>)}
+    </div>
+}
 
 export default LobbyPage;
