1071072f0443dbbcee5e8a74daae16b4de028698
ImMorek <102058631+ImMorek@users.noreply.github.com>
2024-07-30 23:17:04 +0200

    BARTENDER animations :3

--- src/components/models/Bartender.jsx
+++ src/components/models/Bartender.jsx
@@ -4,10 +4,33 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import { WebsocketContext } from '../../utilComponents/WebsocketProvider';
 import { useSpring, animated } from '@react-spring/three'
 
+
 export default function Bartender() {
     const bartender =  useLoader(THREE.TextureLoader, '/images/bartender/bartender.png')
     const bartenderArm =  useLoader(THREE.TextureLoader, '/images/bartender/bartenderArm.png')
     const cardScale = 0.05;
+
+    const {armRotation, armPosition} = useSpring({
+        from: {
+            armRotation: 0,
+            armPosition: -3
+          },
+          to: 
+          [{
+            armRotation: 0.1,
+            armPosition: -2.95
+
+          },
+          {
+            armRotation: 0,
+            armPosition: -3
+          },],
+          config: {
+            friction: 80,
+            tension: 1000,
+          },
+          loop: true
+    })
 
       //init pos [-1, 2.8, -4]
       //after pos [0, 2.6. 0]
@@ -15,9 +38,10 @@ export default function Bartender() {
     return (
         <>
 
-        <mesh
+        <animated.mesh
         position={[9, 4.4, -3]}
         rotation={[0, 4.5, 0]}
+        position-z={armPosition}
         ><>
         <planeGeometry args={[3, 5]} />   
         <meshStandardMaterial 
@@ -29,11 +53,13 @@ export default function Bartender() {
             
             />
             </>
-        </mesh>
+        </animated.mesh>
         
-        <mesh
+        <animated.mesh
         position={[9, 4.4, -3]}
         rotation={[0, 4.5, 0]}
+        rotation-x={armRotation}
+        position-z={armPosition}
         ><>
         <planeGeometry args={[3, 5]} />   
         <meshStandardMaterial 
@@ -46,7 +72,7 @@ export default function Bartender() {
             depthTest={false} // Disable depth testing
             />
             </>
-        </mesh>
+        </animated.mesh>
       
         </>
       )
