f18ad13303964011ca0fcb0285edf8f965e7bd25
JanSkla <jan.skladal11@gmail.com>
2024-09-17 12:27:21 +0200

    shoot death tilt

--- src/components/models/MainCamera.jsx
+++ src/components/models/MainCamera.jsx
@@ -24,6 +24,9 @@ const MainCamera = () => {
   }, [])
 
   useEffect(() => {
+    if(data?.type === 'load-game'){
+      currentRot.current = originalRot;
+    }
     if(data?.type === "round-actions" && data?.data){
     data.data.forEach(action => {
       if(action.user == thisPID){
@@ -70,23 +73,25 @@ const MainCamera = () => {
         
         switch (action.type) {
           case "shoot-death":
+            setTimeout(() => {
+              currentRot.current = [-3.008, -0.23,-3];
+            }, 1000);
+            break
           case "shoot-damage":
           case "shoot-block":
+              setTimeout(() => {
+                currentRot.current[0] = -3.158;
+              }, 1000);
+              setTimeout(() => {
+                currentRot.current[0] = -3.108;
+              }, 1100);
             break;
           case "shoot-drinking-beer":
-              
-            const tId2 = setTimeout(() => {
-              currentRot.current = [-3.308, -0.271, -3.133];
-            }, 1200);
-            const tId3 = setTimeout(() => {
-              currentRot.current = originalRot;
-            }, 2000);
-        
-            // Cleanup function to clear the timeout if the component unmounts
-            return () => {
-              clearTimeout(tId2);
-              clearTimeout(tId3);
-            }
+
+            
+            setTimeout(() => {
+              currentRot.current[0] = -3.358;
+            }, 1300);
             break;
         }
       }
@@ -98,9 +103,14 @@ const MainCamera = () => {
 
   useFrame(({ clock }) => {
     if (!cameraRef.current) return;
-
-    cameraRef.current.rotation.x = currentRot.current[0] - (-Math.sign(cursor.current.y) + Math.sign(cursor.current.y) * Math.pow(cursor.current.y - Math.sign(cursor.current.y), 2)) * MULTIPLIER;
-    cameraRef.current.rotation.y = currentRot.current[1] - (-Math.sign(cursor.current.x) + Math.sign(cursor.current.x) * Math.pow(cursor.current.x - Math.sign(cursor.current.x), 2)) * MULTIPLIER;
+    // cameraRef.current.rotation.x = -3.008
+    // cameraRef.current.rotation.y = -0.23
+    // cameraRef.current.rotation.z = -3
+    cameraRef.current.rotation.x = currentRot.current[0]
+     - (-Math.sign(cursor.current.y) + Math.sign(cursor.current.y) * Math.pow(cursor.current.y - Math.sign(cursor.current.y), 2)) * MULTIPLIER;
+    cameraRef.current.rotation.y = currentRot.current[1]
+     - (-Math.sign(cursor.current.x) + Math.sign(cursor.current.x) * Math.pow(cursor.current.x - Math.sign(cursor.current.x), 2)) * MULTIPLIER;
+     cameraRef.current.rotation.z = currentRot.current[2]
   })
 
   return <PerspectiveCamera ref={cameraRef} makeDefault={true} far={1000} near={0.1} fov={53.702} position={[-1.4, 4.266, -5.225]} rotation={[-3.108, -0.271, -3.133]} scale={1.241} />
